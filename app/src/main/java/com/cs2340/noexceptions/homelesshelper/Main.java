36a60470ce4b227bca47404d1457280461fe09c7
Sc0urg3r <Armad_R@gatech.edu>
2018-02-14 13:59:33 -0500

    Added Log Out Button

--- app/src/main/java/com/cs2340/noexceptions/homelesshelper/Main.java
+++ app/src/main/java/com/cs2340/noexceptions/homelesshelper/Main.java
@@ -1,7 +1,9 @@
 package com.cs2340.noexceptions.homelesshelper;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Main extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class Main extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void toHomeScreen(View v) {
+        Intent toHome = new Intent(getBaseContext(), Home.class);
+        startActivity(toHome);
     }
 }
